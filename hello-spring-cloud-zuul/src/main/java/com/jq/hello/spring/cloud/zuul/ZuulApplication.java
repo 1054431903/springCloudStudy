dc7479a3434b188bad6c9da6998d162c177057f9
jiaqi <jiaqi@zenmen.com>
2019-02-13 14:32:46 +0800

    使用路由网关统一访问接口 fix a bug(spring boot 与  spring cloud 版本不对应造成的异常报错问题)

---
+++ hello-spring-cloud-zuul/src/main/java/com/jq/hello/spring/cloud/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.jq.hello.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author: jq
+ * @Date: 2019/2/12 16:37
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
